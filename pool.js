468f2420b92031f194cd29576faf79b0ad678714
Mihle Aviwe <mihleaviwe6@gmail.com>
2026-04-22 12:32:04 +0200

    update pool.js to include DB_PORT

--- pool.js
+++ pool.js
@@ -13,6 +13,7 @@ console.log('DB connection config:', {
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
+  port: process.env.DB_PORT,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
