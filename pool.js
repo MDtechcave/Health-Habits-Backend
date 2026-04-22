abb556ba95fdc6118c2d37fa34ce50b89f2545b9
Mihle Aviwe <mihleaviwe6@gmail.com>
2026-04-22 13:09:14 +0200

    fix start script and port fallback

--- pool.js
+++ pool.js
@@ -13,7 +13,7 @@ console.log('DB connection config:', {
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  port: process.env.DB_PORT,
+  port: process.env.DB_PORT || 3306,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
